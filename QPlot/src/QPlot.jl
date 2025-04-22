f79e0c71ebe9db7f956b8b833f6d476e20aa592c
Andrea Baroffio <andrea.baroff2000@gmail.com>
2025-04-22 14:39:32 +0200

    minor fix

--- QPlot/src/QPlot.jl
+++ QPlot/src/QPlot.jl
@@ -7,15 +7,13 @@ using LinearAlgebra
 using JSON
 using QEngine
 
-export plot_sdf, plot_thermal_sdf, plot_chain_occupation
 include("sdf.jl")
 
 include("figs.jl")
 
-export animate_chain_occupation, animate_envmodes_occupation
 include("envoccupation.jl")
 
-export savefigs
+export savefigs, save_Ks, save_envmodes
 include("savefigs.jl")
 
 end
