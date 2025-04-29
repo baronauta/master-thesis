6852e0a481d8a7977b98e011a9a38bb728ddd114
Andrea Baroffio <andrea.baroff2000@gmail.com>
2025-04-29 19:38:24 +0200

    minor fix

--- QEngine/src/constants.jl
+++ QEngine/src/constants.jl
@@ -1,19 +1,49 @@
-# Pauli basis
-const σ0 = Complex{Float64}[1.0 0.0; 0.0 1.0]   # Identity matrix
-const σ1 = Complex{Float64}[0.0 1.0; 1.0 0.0]   # Pauli-X
-const σ2 = Complex{Float64}[0.0 -im; im 0.0]    # Pauli-Y
-const σ3 = Complex{Float64}[1.0 0.0; 0.0 -1.0]  # Pauli-Z
-const pauli_basis = [σ0, σ1, σ2, σ3]
+"""
+Pauli basis for 2×2 Hermitian operators.
 
-# Computational basis
+Defines the set of Pauli matrices {σ₀, σ₁, σ₂, σ₃}, where:
+- σ₀ is the 2×2 identity matrix,
+- σ₁ is the Pauli-X matrix,
+- σ₂ is the Pauli-Y matrix,
+- σ₃ is the Pauli-Z matrix.
+
+These matrices form an orthogonal basis for 2×2 Hermitian matrices.
+They are not orthonormal with reference to the Hilbert-Schmidt inner product 
+defined as <A,B> = Tr[A†B]. Their normalized version is obtained considering
+σₖ → σₖ/√2.
+"""
+const σ0 = Matrix{ComplexF64}([1.0 0.0; 0.0 1.0])   # Identity matrix
+const σ1 = Matrix{ComplexF64}([0.0 1.0; 1.0 0.0])   # Pauli-X
+const σ2 = Matrix{ComplexF64}([0.0 -im; im 0.0])    # Pauli-Y
+const σ3 = Matrix{ComplexF64}([1.0 0.0; 0.0 -1.0])  # Pauli-Z
+const paulimatrices = [σ0, σ1, σ2, σ3]
+
+"""
+Canonical operator basis for a two-level quantum system.
+
+Defines the set of 2×2 matrices {T₀₀, T₀₁, T₁₀, T₁₁}, where each Tᵢⱼ corresponds to the operator
+|i⟩⟨j| in Dirac notation. These matrices form a complete basis for the space of 2×2 matrices.
+These matrices provide an orthonormal basis with reference to the Hilbert-Schmidt inner product 
+defined as <A,B> = Tr[A†B].
+
+Explicitly:
+T₀₀ = |0⟩⟨0| = [1 0; 0 0]  
+T₀₁ = |0⟩⟨1| = [0 1; 0 0]  
+T₁₀ = |1⟩⟨0| = [0 0; 1 0]  
+T₁₁ = |1⟩⟨1| = [0 0; 0 1]
+
+Grouped in the list `canonical_op_basis = [T₀₀, T₀₁, T₁₀, T₁₁]`.
+"""
 const T00 = Matrix{ComplexF64}([1.0 0.0; 0.0 0.0])
 const T01 = Matrix{ComplexF64}([0.0 1.0; 0.0 0.0])
 const T10 = Matrix{ComplexF64}([0.0 0.0; 1.0 0.0])
 const T11 = Matrix{ComplexF64}([0.0 0.0; 0.0 1.0])
-const T_basis = [T00, T01, T10, T11]
+const canonical_op_basis = [T00, T01, T10, T11]
 
-# Permutation matrix to reshuffle a A-form matrix into a B-form matrix.
-# For reference see V. Jagadish and F. Petruccione, “An invitation to quantum channels”.
+"""
+Permutation matrix to reshuffle a A-form matrix into a B-form matrix.
+For reference see V. Jagadish and F. Petruccione, “An invitation to quantum channels”.
+"""
 #! format: off
 const A2Bmatrix = Matrix{ComplexF64}([
 #    1  2  3  4   5  6  7  8   9 10 11 12  13 14 15 16
